7e0f38eaf2643889235275ba69bbae38eb5aa4a4
Abhishek Kumar <ak00873@gmail.com>
2025-03-14 12:31:02 +0530

    navbar title change

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (
         <nav className="bg-teal-950 p-4">
             <div className="container mx-auto flex justify-between items-center">
-                <div className="text-white text-lg font-bold">Expense Tracker</div>
+                <div className="text-white text-lg font-bold">Expense Tracker APP</div>
                 <ul className="flex space-x-4">
                     <li><a href="/" className="text-white hover:text-gray-200">Home</a></li>
                     <li><a href="/" className="text-white hover:text-gray-200">About</a></li>
